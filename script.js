178bfec06d1ffb29df0431225a55ccb8cc63a3d7
rdynxyz <radyan6661@gmail.com>
2022-12-16 18:21:40 +0700

    responsive created

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function searchMovie(){
                 let movies = result.Search;
                 $.each(movies, function(i, data){
                     $('#movie-list').append(`
-                    <div class="col-span-1 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <d iv class="col-span-1 bg-gray-800 border rounded-lg shadow-md border-gray-700">
                         <div class="h-3/4 relative overflow-hidden">
                             <img class="rounded-t-lg w-full " src="${data.Poster}"/>
                         </div>
                         <div class="px-5 pt-5">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${data.Title}</h5>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${data.Year}</p>
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-white">${data.Title}</h5>
+                            <p class="mb-3 font-normal text-gray-400">${data.Year}</p>
                             <label for="my-modal" class="see-detail cursor-pointer text-sm font-medium text-center text-blue-500 rounded-lg" data-id="${data.imdbID}"> Detail </label>
                         </div>
                     </div>`)
